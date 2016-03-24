61f749fe7b3d82904a98199c3ba77f8cab9f1a2d
sukso96100 <sukso96100@gmail.com>
2016-03-25 08:39:11 +0900

    Added Homeworks(#1~#2)

---
+++ 201632034_1/src.c
@@ -0,0 +1,12 @@
+//Authored by Youngbin Han<sukso96100@gmail.com><201632034> at 2016.03.18
+//Assignment #1
+
+#include <stdio.h>
+
+int main(void) {
+	printf("제 이름은 한영빈 입니다.\
+			\n 저의 전공학과는 소프트웨어공학과 이고요.\
+			\n 제 학번은 201632034 입니다.\
+			\n 만나서 반갑고 잘 부탁드립니다^^");
+	return 0;
+}
